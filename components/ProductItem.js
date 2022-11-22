bed3ddd17b72d0ae0a9c51af391a017d1789b44c
AnushaDevaiah <anusha.devaiah@wyzebulb.com>
2022-11-22 12:07:24 +0530

    Created products list page and details page

---
+++ components/ProductItem.js
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import React from 'react';
+
+//passing product//product component//import this in pages-index.js
+
+export default function ({ product }) {
+  return (
+    <div className="card">
+      <Link legacyBehavior href={`/product/${product.slug}`}>
+        <a>
+          <img
+            src={product.image}
+            alt={product.name}
+            className="rounded shadow"
+          />
+        </a>
+      </Link>
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link legacyBehavior href={`/product/${product.slug}`}>
+          <a>
+            <p className="text-lg ">{product.name}</p>
+          </a>
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>${product.price}</p>
+        <button className="primary-button" type="button">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
