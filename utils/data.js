171582ead649fb77f97a0d69c92f721222fdac7a
AnushaDevaiah <anusha.devaiah@wyzebulb.com>
2022-11-23 15:39:56 +0530

    feat: :zap: Connection to mongodb
    Connection to mongodb

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,21 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'John', //admin user
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Jane', //normal user
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: 'false',
+    },
+  ],
+
   products: [
     //1st product
     {
