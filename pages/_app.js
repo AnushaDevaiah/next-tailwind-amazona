c46e747182c98de728caab2d9721e8e59e07dce2
AnushaDevaiah16 <anusha.devaiah@codosphere.com>
2022-12-06 18:17:43 +0530

    feat: :zap: pay order by pay pal
    pay order by pay pal

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,21 @@ import '../styles/globals.css';
 import { SessionProvider, useSession } from 'next-auth/react';
 import { StoreProvider } from '../utils/Store';
 import { useRouter } from 'next/router';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <StoreProvider>
-        {Component.auth ? (
-          <Auth>
+        <PayPalScriptProvider deferLoading={true}>
+          {Component.auth ? (
+            <Auth>
+              <Component {...pageProps} />
+            </Auth>
+          ) : (
             <Component {...pageProps} />
-          </Auth>
-        ) : (
-          <Component {...pageProps} />
-        )}
+          )}
+        </PayPalScriptProvider>
       </StoreProvider>
     </SessionProvider>
   );
@@ -27,7 +30,6 @@ function Auth({ children }) {
       router.push('/unauthorized?message=login required');
     },
   });
-
   if (status === 'loading') {
     return <div>Loading...</div>;
   }
